efd63c8af0a1211e9dff24f412647d93cb956163
Cvelth <Cvelth.mail@gmail.com>
2017-09-18 16:02:32 +0300

    TaskProcessor was implemented.

--- Lab1/GUI/GUI.cpp
+++ Lab1/GUI/GUI.cpp
@@ -8,5 +8,8 @@ GUI::GUI(QWidget *parent)
 	cs::ProcessorSimulator p;
 	p.initialize(cs::StorageType::LIFO);
 	p.changeLambda(2.3);
+	p.changeMu(5.2);
+	p.changeSigma(1.0);
+	p.changeTau(1.0);
 	p.start();
 }
--- Lab1/Simulation/TaskGenerator.cpp
+++ Lab1/Simulation/TaskGenerator.cpp
@@ -8,16 +8,16 @@ void cs::TaskGenerator::loop() {
 	std::thread t([this]() {
 		std::random_device seed;
 		std::mt19937_64 g(seed());
-		std::poisson_distribution<size_t> *d;
+		std::poisson_distribution<float> *d;
 		while (m_state == SimulationState::Running || m_state == SimulationState::Paused) 
 			if (m_state == SimulationState::Running) {
 				if (!d || d->mean() != m_lambda)
-					d = new std::poisson_distribution<size_t>(m_lambda);
+					d = new std::poisson_distribution<float>(m_lambda);
 				auto wait = (*d)(g);
 				std::this_thread::sleep_for(std::chrono::duration<float>(
 					*m_time_coefficient * cs::constants::time_correction * wait));
 				m_storage->push();
 			}
 	});
-	t.join();
+	t.detach();
 }
