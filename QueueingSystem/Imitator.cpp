bf98dc4cdd6fed97722c683894daa345623967f9
Cvelth <Cvelth.mail@gmail.com>
2017-10-10 18:25:41 +0300

    Average_processing_time is now being calculated.

--- QueueingSystem/Imitator.cpp
+++ QueueingSystem/Imitator.cpp
@@ -7,7 +7,7 @@ Time start_point = 0.f;
 Time max = std::numeric_limits<Time>::max();
 
 void qs::QueueingSystemImitation::run(size_t tasks) {
-	std::thread t([&tasks, this]() {
+	std::thread t([tasks, this]() {
 		std::random_device seed;
 		std::mt19937_64 g(seed());
 		std::poisson_distribution<number> p(m_lambda);
@@ -16,9 +16,10 @@ void qs::QueueingSystemImitation::run(size_t tasks) {
 		Time current_time = start_point;
 		size_t tasks_generated = 0u;
 		size_t tasks_processed = 0u;
+		Time tasks_processing_time = 0u;
 		Time next_generation = p(g);
 		Time next_processing = m_tau;
-		while (tasks_processed <= tasks) {
+		while (tasks_processed < tasks) {
 			if (next_generation < next_processing) {
 				current_time = next_generation;
 				m_storage->push(new TaskImitation(current_time, n(g)));
@@ -31,13 +32,18 @@ void qs::QueueingSystemImitation::run(size_t tasks) {
 					if (task.length > m_tau) {
 						task.length -= m_tau;
 						m_storage->repush(&task);
-					} else
+					} else {
 						tasks_processed++;
+						tasks_processing_time += current_time - task.generated;
+					}
 				} catch (qs::Exceptions::EmptyQueue) {}
 				next_processing += m_tau;
 			}
 		}
-		current_time = next_generation;
+		auto average_processing_time = tasks_processing_time / tasks_processed;
+		current_time = max;
 	});
 	t.detach();
 }
+
+//Git_hub_test
