c075d498231e7d17debde6f187ff2bd210525d93
Cvelth <cvelth.mail@gmail.com>
2017-11-14 17:17:15 +0200

    PetriNet core was implemented.

--- PetriNet/PetriNet.hpp
+++ PetriNet/PetriNet.hpp
@@ -1,1 +1,28 @@
 #pragma once
+#include <list>
+#include <map>
+namespace pn {
+	template <typename TaskType, size_t Cores = 1>
+	class PetriNet {
+	private:
+		float m_tau;
+		std::list<TaskType> m_awaiting;
+		std::shared_ptr<TaskType> m_executing[Cores];
+		std::map<PetriNet*, size_t> m_transitions;
+	public:
+		PetriNet(float tau) : m_tau(tau) {}
+		void insert(TaskType const& task) {
+			m_awaiting.push_back(task);
+		}
+		void tau() const { return m_tau; }
+		void step(float &time) {
+			for (size_t i = 0; i < Cores; i++)
+				if (!m_awaiting.empty())
+					if (!m_executing[i]) {
+						m_executing[i] = m_awaiting.front();
+						m_awaiting.pop_front();
+					}
+			time += m_tau;
+		}
+	};
+}
---
+++ PetriNet/lib.cpp
@@ -0,0 +1,1 @@
+#include "PetriNet.hpp"
