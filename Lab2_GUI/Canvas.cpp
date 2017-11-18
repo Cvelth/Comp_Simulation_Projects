2ca80b5f62b27f754cbe42ca6bf88ebe729c2212
Cvelth <cvelth.mail@gmail.com>
2017-11-18 20:54:02 +0200

    Basic graphics pipeline was added to Canvas.

--- Lab2_GUI/Canvas.cpp
+++ Lab2_GUI/Canvas.cpp
@@ -1,30 +1,151 @@
 #include "Canvas.hpp"
-Canvas::Canvas(QWidget *parent)	: QOpenGLWidget(parent) {
+#define _USE_MATH_DEFINES
+#include "Math.h"
+#include <QMatrix4x4>
+Canvas::Canvas(QWidget *parent)	: QOpenGLWidget(parent), m_projection(nullptr) {
+	insertNet(std::make_shared<Net>(1.f));
 }
 Canvas::~Canvas() {
+	if (m_projection) delete m_projection;
 }
-void Canvas::initilizeNets() {
-	for (auto it : m_nets) {
-
-	}
+void Canvas::insertNet(NetType const net) {
+	m_nets.insert(std::make_pair(net, std::make_tuple(false, 0, 0, 0)));
+}
+constexpr size_t Size = 24;
+constexpr size_t Coords = 2;
+float res[Size * Coords];
+void Canvas::initializeNets() {
+	for (auto& it : m_nets)
+		if (!std::get<0>(it.second)) {
+			glGenBuffers(1, &std::get<1>(it.second));
+			glBindBuffer(GL_ARRAY_BUFFER, std::get<1>(it.second));
+			size_t i = 0;
+			for (float f = 0.f; f <= M_PI * 2.1f; f += M_PI / Size * 2) {
+				res[i++] = 0.3f * cosf(f);
+				res[i++] = 0.3f * sinf(f);
+			}
+			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * Size * Coords, res, GL_STATIC_DRAW);
+			std::get<0>(it.second) = true;
+		}
 }
 void Canvas::draw(std::pair<NetType const, NetInfo>& net) {
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, std::get<1>(net.second));
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*) 0);
 
+	glDrawArrays(GL_LINE_LOOP, 0, Size);
+	glDisableVertexAttribArray(0);
+}
+
+const char* vertex_source = ""
+	"#version 330 core\n"
+	""
+	"layout(location = 0) in vec2 position;"
+	""
+	"out vec3 fragment_color;"
+	""
+	"uniform vec3 color;"
+	"uniform vec2 translation;"
+	""
+	"void main(){"
+	"	gl_Position.xy = position + translation;"
+	"	gl_Position.z = 0.0;"
+	"	gl_Position.w = 1.0;"
+	""
+	"	fragment_color = color;"
+	"}";
+const char* fragment_source = ""
+"#version 330 core\n"
+	""
+	"in vec3 fragment_color;"
+	"out vec3 color;"
+	""
+	"void main() {"
+	"	color = fragment_color;"
+	"}";
+enum class ShaderType {
+	Vertex, Fragment
+};
+GLuint Canvas::compileSource(ShaderType type, const char* sourceText) {
+	const GLchar* source = static_cast<const GLchar*>(sourceText);
+	if (source == "") throw std::exception("The source string or file is empty.");
+
+	GLuint id;
+	if (type == ShaderType::Vertex)
+		id = glCreateShader(GL_VERTEX_SHADER);
+	else if (type == ShaderType::Fragment)
+		id = glCreateShader(GL_FRAGMENT_SHADER);
+	else
+		throw std::exception("Enum class ShaderType seems to be broken.");
+
+	glShaderSource(id, 1, &source, NULL);
+	glCompileShader(id);
+
+	GLint isCompiled;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &isCompiled);
+
+	if (!isCompiled) {
+		GLsizei len;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &len);
+
+		GLchar* log = new GLchar[len + 1];
+		glGetShaderInfoLog(id, len, &len, log);
+		throw std::exception(("Shader compilation error: " + std::string(log)).c_str());
+		delete[] log;
+	}
+	return id;
+}
+GLuint Canvas::link(const std::initializer_list<GLuint>& shaders) {
+	if (shaders.size() == 0u)
+		throw std::exception("There is no shaders to attach");
+
+	GLuint id = glCreateProgram();
+
+	for (auto shader : shaders)
+		glAttachShader(id, shader);
+
+	glLinkProgram(id);
+
+	GLint isLinked;
+	glGetProgramiv(id, GL_LINK_STATUS, &isLinked);
+	if (!isLinked) {
+		GLsizei len;
+		glGetProgramiv(id, GL_INFO_LOG_LENGTH, &len);
+
+		GLchar* log = new GLchar[len + 1];
+		glGetProgramInfoLog(id, len, &len, log);
+		throw std::exception(("Program linking error: " + std::string(log)).c_str());
+		delete[] log;
+	}
+	return id;
 }
 void Canvas::initializeGL() {
 	initializeOpenGLFunctions();
-	initilizeNets();
+	glGenVertexArrays(1, &m_vertexArray);
+	glBindVertexArray(m_vertexArray);
+	glLineWidth(1.5);
+
+	auto m_program = link({compileSource(ShaderType::Vertex, vertex_source),
+						  compileSource(ShaderType::Fragment, fragment_source)});
+	glUseProgram(m_program);
+
+	GLint loc = glGetUniformLocation(m_program, "color");
+	if (loc != -1) {
+		glUniform3f(loc, 1.f, 0.f, 0.f);
+	} else
+		throw std::exception("Something wrong with uniform allocation.");
 }
 void Canvas::resizeGL(int w, int h) {
-
+	glViewport(0, 0, w, h);
+	if (m_projection) delete m_projection;
+	m_projection = new QMatrix4x4();
+	auto ar = float(w) / h;
+	m_projection->ortho(-ar, +ar, -1.f / ar, +1.f / ar, -1, +1);
 }
 void Canvas::paintGL() {
+	initializeNets();
+
 	glClear(GL_COLOR_BUFFER_BIT);
-	for (auto it : m_nets)
-		if (!std::get<0>(it.second)) {
-			initilizeNets();
-			break;
-		}
 	for (auto& it : m_nets)
 		draw(it);
 }
--- Lab2_GUI/Canvas.hpp
+++ Lab2_GUI/Canvas.hpp
@@ -1,24 +1,35 @@
 #pragma once
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
+#include <qopenglfunctions_3_3_core.h>
 #include <unordered_map>
 #include <memory>
-namespace pn {
-	class PetriNet;
-}
-class Canvas : public QOpenGLWidget, protected QOpenGLFunctions {
+#include "..\PetriNet\PetriNet.hpp"
+using Net = pn::PetriNet<int>;
+class QMatrix4x4;
+enum class ShaderType;
+class Canvas : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core {
 	Q_OBJECT
 private:
-	using NetType = std::shared_ptr<pn::PetriNet>;
-	using NetInfo = std::tuple<bool, size_t, size_t>;
+	using NetType = std::shared_ptr<Net>;
+	using NetInfo = std::tuple<bool, GLuint, size_t, size_t>;
 	std::unordered_map<NetType, NetInfo> m_nets;
+private:
+	GLuint m_vertexArray;
+	GLuint m_program;
+	QMatrix4x4 *m_projection;
 protected:
-	virtual void initilizeNets();
+	virtual void initializeNets();
 	virtual void draw(std::pair<NetType const, NetInfo> &net);
 	virtual void initializeGL() override;
 	virtual void resizeGL(int w, int h) override;
 	virtual void paintGL() override;
+protected:
+	GLuint compileSource(ShaderType type, const char * sourceText);
+	GLuint link(const std::initializer_list<GLuint>& shaders);
 public:
 	Canvas(QWidget *parent = Q_NULLPTR);
 	~Canvas();
+
+	virtual void insertNet(NetType const net);
 };
