4a71100e8403dfd5173f4aab016700b9134df09a
Cvelth <cvelth.mail@gmail.com>
2017-11-23 17:50:25 +0200

    pn::PetriNet was improved. It now stores name (as std::string). Bug in step function was fixed.

--- PetriNet/PetriNet.hpp
+++ PetriNet/PetriNet.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 #include <list>
 #include <vector>
 #include <map>
@@ -7,18 +8,19 @@ namespace pn {
 	template <typename TaskType>
 	class PetriNet {
 	private:
+		std::string m_name;
 		float m_tau;
 		std::list<TaskType> m_awaiting;
 		std::vector<std::shared_ptr<TaskType>> m_executing;
 		std::map<PetriNet*, size_t> m_transitions;
 	public:
-		PetriNet(float tau, size_t cores = 1) : m_tau(tau) { m_executing.resize(cores); }
+		PetriNet(std::string name = ""s, float tau = 1.f, size_t cores = 1) : m_tau(tau) { m_executing.resize(cores); }
 		void insert(TaskType const& task) {
 			m_awaiting.push_back(task);
 		}
 		void tau() const { return m_tau; }
 		void step(float &time) {
-			for (size_t i = 0; i < Cores; i++)
+			for (size_t i = 0; i < m_executing.size(); i++)
 				if (!m_awaiting.empty())
 					if (!m_executing[i]) {
 						m_executing[i] = m_awaiting.front();
