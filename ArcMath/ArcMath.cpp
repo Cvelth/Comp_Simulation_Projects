2dba8aeffd9805ba7759beebde1417b9356964c9
Cvelth <cvelth.mail@gmail.com>
2017-12-05 00:13:34 +0200

    12/5/17.

--- ArcMath/ArcMath.cpp
+++ ArcMath/ArcMath.cpp
@@ -1,4 +1,44 @@
 #include "ArcMath.hpp"
-std::vector<arc_math::number> arc_math::generate(point const& s, point const& e, size_t const accuracy) {
+using namespace arc_math;
+point operator+(point const& s, point const& e) {
+	return point{e.x + s.x, e.y + s.y};
+}
+point operator-(point const& s, point const& e) {
+	return point{e.x - s.x, e.y - s.y};
+}
+point middle(point const& s, point const& e) {
+	return point{(s.x + e.x) / 2, (s.y + e.y) / 2};
+}
+number operator*(point const& p) {
+	return sqrtf(p.x * p.x + p.y * p.y);
+}
+point operator*(point const& p, number const& n) {
+	return point{p.x * n, p.y * n};
+}
+point operator/(point const& p, number const& n) {
+	return point{p.x / n, p.y / n};
+}
+point operator!(point const& p) {
+	return point{-p.x, p.y} / *p;
+}
+point operator~(point const& p) {
+	return p / *p;
+}
+number operator*(point const& s, point const& e) {
+	return s.x * e.x + s.y * e.y;
+}
+std::vector<number> generate(point const& s, point const& e, size_t const accuracy) {
+	 point se = e - s;
+	 point c = middle(s, e);
+	 point n = !se;
+	 point o = c + n * *se;
+
+	 point os = s - o;
+	 point oe = e - o;
+
+	 point i{1, 0};
+	 number as = ~os * i;
+	 number ae = ~oe * i;
+
 	return std::vector<number>{s.x, s.y, e.x, e.y};
 }
