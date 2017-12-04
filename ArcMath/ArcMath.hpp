b5dabff1bc50167b4157a3a315146d991d7177c1
Cvelth <cvelth.mail@gmail.com>
2017-12-04 23:15:49 +0200

    Base header structure for new lib was implemented.

--- ArcMath/ArcMath.hpp
+++ ArcMath/ArcMath.hpp
@@ -1,1 +1,9 @@
 #pragma once
+#include <vector>
+namespace arc_math {
+	using number = float;
+	struct point {
+		number x, y;
+	};
+	std::vector<number> generate(point const& s, point const& e, size_t const accuracy = 20ull);
+}
