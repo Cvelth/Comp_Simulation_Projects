1a517dd059c769b3cecf37b4c12177fd91d3cd57
Cvelth <cvelth.mail@gmail.com>
2017-12-05 18:16:02 +0200

    Arc is now rendered properly.

--- ArcMath/ArcMath.cpp
+++ ArcMath/ArcMath.cpp
@@ -1,5 +1,7 @@
+#include <cmath>
 #include "ArcMath.hpp"
 using namespace arc_math;
+const float pi = std::atanf(1.f) * 4.f;
 point operator+(point const& s, point const& e) {
 	return point{e.x + s.x, e.y + s.y};
 }
@@ -19,7 +21,7 @@ point operator/(point const& p, number const& n) {
 	return point{p.x / n, p.y / n};
 }
 point operator!(point const& p) {
-	return point{-p.x, p.y} / *p;
+	return point{p.y, -p.x};
 }
 point operator~(point const& p) {
 	return p / *p;
@@ -27,18 +29,31 @@ point operator~(point const& p) {
 number operator*(point const& s, point const& e) {
 	return s.x * e.x + s.y * e.y;
 }
-std::vector<number> generate(point const& s, point const& e, size_t const accuracy) {
-	 point se = e - s;
-	 point c = middle(s, e);
-	 point n = !se;
-	 point o = c + n * *se;
+std::vector<number> arc_math::generate(point const& s, point const& e, size_t const accuracy) {
+	std::vector<number> arc;
 
-	 point os = s - o;
-	 point oe = e - o;
+	point se = e - s;
+	point c = middle(s, e);
+	point n = ~!se;
+	point o = c + n * *se;
 
-	 point i{1, 0};
-	 number as = ~os * i;
-	 number ae = ~oe * i;
+	point os = s - o;
+	point oe = e - o;
 
-	return std::vector<number>{s.x, s.y, e.x, e.y};
+	number r = *os;
+
+	number am = acosf(~os * ~oe);
+	number as = acosf(~os * point{1, 0});
+	float step = am / accuracy;
+
+	for (float f = 0.f; f <= am; f += step) {
+		if (s.y < o.y) {
+			arc.push_back(-r * cosf(f + as) + o.x);
+			arc.push_back(-r * sinf(f + as) + o.y);
+		} else {
+			arc.push_back(-r * cosf(f + as -am) + o.x);
+			arc.push_back(r * sinf(f + as - am) + o.y);
+		}
+	}
+	return arc;
 }
