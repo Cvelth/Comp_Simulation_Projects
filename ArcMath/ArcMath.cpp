0ebee6e0b6cf69661dec1e724dc54899f4922f77
Cvelth <cvelth.mail@gmail.com>
2017-12-05 18:29:13 +0200

    Minor code improvements.

--- ArcMath/ArcMath.cpp
+++ ArcMath/ArcMath.cpp
@@ -32,28 +32,26 @@ number operator*(point const& s, point const& e) {
 std::vector<number> arc_math::generate(point const& s, point const& e, size_t const accuracy) {
 	std::vector<number> arc;
 
-	point se = e - s;
-	point c = middle(s, e);
-	point n = ~!se;
-	point o = c + n * *se;
-
+	point o = middle(s, e) + !(e - s);
 	point os = s - o;
-	point oe = e - o;
-
 	number r = *os;
 
-	number am = acosf(~os * ~oe);
+	number am = acosf(~os * ~(e - o));
 	number as = acosf(~os * point{1, 0});
 	float step = am / accuracy;
 
+	float d = (s.y > o.y) ? am : 0.f;
+	float m = (s.y > o.y) ? 1.f : -1.f;
 	for (float f = 0.f; f <= am; f += step) {
-		if (s.y < o.y) {
-			arc.push_back(-r * cosf(f + as) + o.x);
-			arc.push_back(-r * sinf(f + as) + o.y);
-		} else {
-			arc.push_back(-r * cosf(f + as -am) + o.x);
-			arc.push_back(r * sinf(f + as - am) + o.y);
-		}
+			arc.push_back(-r * cosf(f + as - d) + o.x);
+			arc.push_back(m * r * sinf(f + as - d) + o.y);
+	}
+	if (s.y < o.y) {
+		arc.push_back(e.x);
+		arc.push_back(e.y);
+	} else {
+		arc.push_back(s.x);
+		arc.push_back(s.y);
 	}
 	return arc;
 }
