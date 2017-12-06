87e1bdf02e204dc8a42e664fbf0fa9e43dc14ae2
Cvelth <cvelth.mail@gmail.com>
2017-12-06 15:33:16 +0200

    Structure of Lab3_GUI was implemented. Theoretical Calculation button was added.

--- Lab3_GUI/gui.cpp
+++ Lab3_GUI/gui.cpp
@@ -1,5 +1,19 @@
 #include "gui.hpp"
-gui_l3::gui_l3(QWidget *parent) : gui(parent) {}
+#include <QPushButton>
+#include "..\PetriNet\PetriNetTheoreticalCalculation.hpp"
+#include "..\Lab2_GUI\Canvas.hpp"
+gui_l3::gui_l3(QWidget *parent) : gui(parent) {
+	ui.buttons->addWidget(m_b = new QPushButton("Theoretical Calculation"));
+	connect(m_b, &QPushButton::clicked, [this]() {
+		std::vector<size_t> tasks;
+		for (size_t i = 0u; i <= ui.tasks->value(); i++) tasks.push_back(i);
+		auto result = pn::PetriNetTheoreticalCalculation<size_t>::calculate(m_canvas->nets(), tasks);
+	});
+}
 gui_l3::~gui_l3() {
+	delete m_b;
+	delete m_canvas;
+	delete m_net;
+	delete m_link;
 	exit(0);
 }
--- Lab3_GUI/gui.hpp
+++ Lab3_GUI/gui.hpp
@@ -1,8 +1,11 @@
 #pragma once
 #include "..\Lab2_GUI\gui.hpp"
+class QPushButton;
 class gui_l3 : public gui {
 	Q_OBJECT
 public:
 	gui_l3(QWidget *parent = Q_NULLPTR);
 	virtual ~gui_l3();
+protected:
+	QPushButton *m_b;
 };
