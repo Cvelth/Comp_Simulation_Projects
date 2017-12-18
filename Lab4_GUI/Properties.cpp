fd5da2f9aa54eecfa184770188f752c7224d41b3
Cvelth <cvelth.mail@gmail.com>
2017-12-19 01:50:30 +0200

    Properties were updated for Lab4_GUI.

--- Lab4_GUI/Properties.cpp
+++ Lab4_GUI/Properties.cpp
@@ -1,66 +1,8 @@
 #include "Properties.hpp"
-Usage::Usage(QString name, QString value, QWidget *parent) {
+LinkWidget::LinkWidget(City first, City second, QWidget *parent) : QDialog(parent) {
 	ui.setupUi(this);
-	ui.label->setText(name);
-	ui.value->setText(value);
-	ui.value->setReadOnly(true);
-}
-Usage::~Usage() {}
-#include "..\PetriNet\PetriNet.hpp"
-NetDialog::NetDialog(QWidget *parent) : QDialog(parent) {
-	ui.setupUi(this);
-	ui.layout->addWidget(&w);
-	connect(ui.ok, &QPushButton::clicked, [this]() {
-		emit newNetCreated(w.network());
-		close();
-	});
-	connect(ui.cancel, &QPushButton::clicked, [this]() {
-		close();
-	});
-}
-NetDialog::~NetDialog() {}
-NetWidget::NetWidget(QWidget *parent) : QWidget(parent), m_update(nullptr) {
-	ui.setupUi(this);
-}
-NetWidget::~NetWidget() {
-	for (auto it : m_usage)
-		delete it;
-	if (m_update)
-		delete m_update;
-}
-Canvas::NetType NetWidget::network() {
-	return std::make_shared<Net>(ui.name->text().toStdString(), float(ui.tau->value()), size_t(ui.cores->value()));
-}
-void NetWidget::select(std::string name, size_t cores, float tau, std::vector<float> usage, float queue) {
-	ui.name->setText(QString::fromStdString(name));
-	ui.cores->setValue(cores);
-	ui.tau->setValue(tau);
-	ui.queue->setText(QString::number(queue));
-
-	for (auto it : m_usage)
-		delete it;
-	m_usage.clear();
-	if (cores == 1) {
-		m_usage.push_back(new Usage("Usage", QString::number(usage[0] * 100) + "%"));
-		ui.usage->addWidget(m_usage.front());
-	} else
-		for (size_t i = 0; i < usage.size(); i++) {
-			m_usage.push_back(new Usage("Usage of core #" + QString::number(i), QString::number(usage[i] * 100) + "%"));
-			ui.usage->addWidget(m_usage[i]);
-		}
-	if (m_update)
-		delete m_update;
-	m_update = new QPushButton("Update");
-	connect(m_update, &QPushButton::clicked, [this]() {
-		emit updated(ui.name->text().toStdString(), ui.cores->value(), ui.tau->value());
-	});
-	ui.usage->addWidget(m_update);
-	show();
-}
-LinkWidget::LinkWidget(std::string first, std::string second, QWidget *parent) : QDialog(parent) {
-	ui.setupUi(this);
-	ui.first->setText(QString::fromStdString(first));
-	ui.second->setText(QString::fromStdString(second));
+	ui.to_second_label->setText(QString::number(first) + " -> " + QString::number(second));
+	ui.to_first_label->setText(QString::number(second) + " -> " + QString::number(first));
 	connect(ui.save, &QPushButton::clicked, [this]() { 
 		emit updated(to_second(), to_first());
 		close();
@@ -69,9 +11,9 @@ LinkWidget::LinkWidget(std::string first, std::string second, QWidget *parent) :
 LinkWidget::~LinkWidget() {}
 float LinkWidget::to_second() { return ui.to_second->value(); }
 float LinkWidget::to_first() { return ui.to_first->value(); }
-void LinkWidget::select(std::string first_name, std::string second_name, float to_second, float to_first) {
-	ui.first->setText(QString::fromStdString(first_name));
-	ui.second->setText(QString::fromStdString(second_name));
+void LinkWidget::select(City from, City to, float to_second, float to_first) {
+	ui.to_second_label->setText(QString::number(first) + " -> " + QString::number(second));
+	ui.to_first_label->setText(QString::number(second) + " -> " + QString::number(first));
 	ui.to_second->setValue(to_second);
 	ui.to_first->setValue(to_first);
 	show();
--- Lab4_GUI/Properties.hpp
+++ Lab4_GUI/Properties.hpp
@@ -1,38 +1,12 @@
 #pragma once
-#include <QWidget>
-#include "ui_Usage.h"
-class Usage : public QWidget {
-	Q_OBJECT
-public:
-	Usage(QString name = "", QString value = "", QWidget *parent = Q_NULLPTR);
-	~Usage();
-private:
-	Ui::Usage ui;
-};
-#include "Canvas.hpp"
-#include "ui_NetWidget.h"
-class QPushButton;
-class NetWidget : public QWidget {
-	Q_OBJECT
-public:
-	NetWidget(QWidget *parent = Q_NULLPTR);
-	~NetWidget();
-	Canvas::NetType network();
-private:
-	Ui::NetWidget ui;
-	std::vector<Usage*> m_usage;
-	QPushButton *m_update;
-public slots:
-	void select(std::string name, size_t cores, float tau, std::vector<float> usage, float queue);
-signals:
-	void updated(std::string name, size_t cores, float tau);
-};
+#include "..\DynamicProgramming\TravelingSalesmanProblem.hpp"
+using namespace dp::TravelingSalesmanProblem;
 #include <QDialog>
 #include "ui_LinkWidget.h"
 class LinkWidget : public QDialog {
 	Q_OBJECT
 public:
-	LinkWidget(std::string from, std::string to, QWidget *parent = Q_NULLPTR);
+	LinkWidget(City first, City second, QWidget *parent = Q_NULLPTR);
 	~LinkWidget();
 	float to_second();
 	float to_first();
@@ -41,17 +15,5 @@ private:
 signals:
 	void updated(float, float);
 public slots:
-	void select(std::string first_name, std::string second_name, float to_second, float to_first);
-};
-#include "ui_WidgetDialog.h"
-class NetDialog : public QDialog {
-	Q_OBJECT
-public:
-	NetDialog(QWidget *parent = Q_NULLPTR);
-	~NetDialog();
-private:
-	Ui::WidgetDialog ui;
-	NetWidget w;
-signals:
-	void newNetCreated(Canvas::NetType);
+	void select(City first, City second, float to_second, float to_first);
 };
