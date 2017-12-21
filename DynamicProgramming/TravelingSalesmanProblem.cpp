20325910f19105f5ee9a699e468291fe4e8b06cd
Cvelth <cvelth.mail@gmail.com>
2017-12-22 01:14:02 +0200

    Second method was successfully implemented for Lab4.

--- DynamicProgramming/TravelingSalesmanProblem.cpp
+++ DynamicProgramming/TravelingSalesmanProblem.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <algorithm>
 #include <numeric>
+#include <memory>
 std::map<std::vector<dp::TravelingSalesmanProblem::City>, float>& merge(std::map<std::vector<dp::TravelingSalesmanProblem::City>, float> &first, std::map<std::vector<dp::TravelingSalesmanProblem::City>, float> second) {
 	for (auto it : second) {
 		if (first.find(it.first) == first.end())
@@ -39,6 +40,74 @@ std::vector<dp::TravelingSalesmanProblem::City> dp::TravelingSalesmanProblem::so
 		}
 	return output;
 }
+bool is_ignored(size_t i, size_t j, std::vector<std::pair<dp::TravelingSalesmanProblem::City, dp::TravelingSalesmanProblem::City>> ignored) {
+	return i != j && std::find_if(ignored.begin(), ignored.end(), [&i, &j](std::pair<dp::TravelingSalesmanProblem::City, dp::TravelingSalesmanProblem::City> &it) { 
+		return it.first == i || it.second == j || (it.first == j && it.second == i);
+	}) == ignored.end();
+}
+std::vector<dp::TravelingSalesmanProblem::City> solveTSP_2(std::vector<std::vector<dp::TravelingSalesmanProblem::Distance>> const& input_matrix, 
+														   std::vector<std::pair<dp::TravelingSalesmanProblem::City, dp::TravelingSalesmanProblem::City>> ignored = {}, 
+														   std::shared_ptr<std::vector<dp::TravelingSalesmanProblem::City>> res = std::make_shared<std::vector<dp::TravelingSalesmanProblem::City>>())
+{
+	if (res->size() >= input_matrix.size())
+		return *res;
+
+	size_t size = input_matrix.size();
+	std::vector<std::vector<dp::TravelingSalesmanProblem::Distance>> modified = input_matrix;
+	for (size_t i = 0; i < size; i++) {
+		dp::TravelingSalesmanProblem::Distance min = std::numeric_limits<dp::TravelingSalesmanProblem::Distance>::max();
+		for (size_t j = 0; j < size; j++)
+			if (is_ignored(i, j, ignored))
+				if (modified[i][j] < min) {
+					min = modified[i][j];
+				}
+		for (size_t j = 0; j < size; j++)
+			if (is_ignored(i, j, ignored))
+				modified[i][j] -= min;
+	}
+
+	for (size_t j = 0; j < size; j++) {
+		dp::TravelingSalesmanProblem::Distance min = std::numeric_limits<dp::TravelingSalesmanProblem::Distance>::max();
+		for (size_t i = 0; i < size; i++)
+			if (is_ignored(i, j, ignored))
+				if (modified[i][j] < min)
+					min = modified[i][j];
+		for (size_t i = 0; i < size; i++)
+			if (is_ignored(i, j, ignored))
+				modified[i][j] -= min;
+	}
+
+	dp::TravelingSalesmanProblem::Distance max_zero = std::numeric_limits<dp::TravelingSalesmanProblem::Distance>::min();
+	size_t max_zero_i = 0u, max_zero_j = 0u;
+	for (size_t i = 0; i < size; i++)
+		for (size_t j = 0; j < size; j++)
+			if (is_ignored(i, j, ignored))
+				if (modified[i][j] == 0.f) {
+					dp::TravelingSalesmanProblem::Distance min_x = std::numeric_limits<dp::TravelingSalesmanProblem::Distance>::max();
+					dp::TravelingSalesmanProblem::Distance min_y = std::numeric_limits<dp::TravelingSalesmanProblem::Distance>::max();
+					for (size_t k = 0; k < size; k++) 
+						if (is_ignored(i, j, ignored)) {
+							if (i != k && j != k && modified[i][k] < min_x)
+								min_x = modified[i][k];
+							if (i != k && j != k && modified[k][j] < min_y)
+								min_y = modified[k][j];
+					}
+					if (min_x + min_y > max_zero) {
+						max_zero = min_x + min_y;
+						max_zero_i = i;
+						max_zero_j = j;
+					}
+				}
+
+	ignored.push_back(std::make_pair(max_zero_i, max_zero_j));
+	if (!res->empty() && res->back() == max_zero_i) 
+		res->push_back(max_zero_j);
+	else {
+		res->push_back(max_zero_i);
+		res->push_back(max_zero_j);
+	}
+	return solveTSP_2(input_matrix, ignored, res);
+}
 std::vector<dp::TravelingSalesmanProblem::City> dp::TravelingSalesmanProblem::other_method::solve(std::vector<std::vector<Distance>> const& input_matrix) {
-	return std::vector<City>();
+	return solveTSP_2(input_matrix);
 }
